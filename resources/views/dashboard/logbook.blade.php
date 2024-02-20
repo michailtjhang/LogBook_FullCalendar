15c8034787e3d80f0e33806c5baf0a25972f67d8
michailtjhang <michailaja@gmail.com>
2024-02-20 17:30:53 +0700

    feature/Install & setup fullcalendar & bootstrap 5

--- resources/views/dashboard/logbook.blade.php
+++ resources/views/dashboard/logbook.blade.php
@@ -4,19 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
+  </head>
     <title>Document</title>
 </head>
 <body>
     
-    <div id='calendar'></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 mt-3">
+        <div id='calendar'></div>
+      </div>
+    </div>
+  </div>
 
+    <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
+    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/bootstrap5@6.1.10/index.global.min.js'></script>
     <script>
 
       document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
+          initialView: 'dayGridMonth',
+          themeSystem: 'bootstrap5',
+          events: [
+            {
+              id: 1,
+              title: 'All Day Event',
+              start: '2024-02-02'
+            }
+          ]
         });
         calendar.render();
       });
